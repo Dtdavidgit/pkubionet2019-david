e3ed2981cdd6957bfe0059bbe218b9fcfba3204d
zengdewei <zengdewei@xbiome.cn>
2019-04-28 00:11:50 +0800

    LICENSE

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+/*
+ * Change Navbar color while scrolling
+*/
+
+$(window).scroll(function(){
+	handleTopNavAnimation();
+});
+
+$(window).load(function(){
+	handleTopNavAnimation();
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid');
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid');
+	}
+}
+
+/*
+ * Registration Form
+*/
+
+$('#registration-form').submit(function(e){
+    e.preventDefault();
+
+    var postForm = { //Fetch form data
+            'fname'     : $('#registration-form #fname').val(),
+            'lname'     : $('#registration-form #lname').val(),
+            'email'     : $('#registration-form #email').val(),
+            'cell'      : $('#registration-form #cell').val(),
+            'address'   : $('#registration-form #address').val(),
+            'zip'       : $('#registration-form #zip').val(),
+            'city'      : $('#registration-form #city').val(),
+            'program'   : $('#registration-form #program').val()
+    };
+
+    $.ajax({
+            type      : 'POST',
+            url       : './assets/php/contact.php',
+            data      : postForm,
+            dataType  : 'json',
+            success   : function(data) {
+                            if (data.success) {
+                                $('#registration-msg .alert').html("Registration Successful");
+                                $('#registration-msg .alert').removeClass("alert-danger");
+                                $('#registration-msg .alert').addClass("alert-success");
+                                $('#registration-msg').show();
+                            }
+                            else
+                            {
+                                $('#registration-msg .alert').html("Registration Failed");
+                                $('#registration-msg .alert').removeClass("alert-success");
+                                $('#registration-msg .alert').addClass("alert-danger");
+                                $('#registration-msg').show();
+                            }
+                        }
+        });
+});
+
+/*
+ * SmoothScroll
+*/
+
+smoothScroll.init();
+
+// set the date we're counting down to
+var target_date = new Date('2018-06-09T09:00:00-08:00').getTime();
+
+// variables for time units
+var days, hours, minutes, seconds;
+
+// get tag element
+var countdown = document.getElementById('countdown');
+
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+
+    // format countdown string + set tag value
+		if (seconds_left > 0) {
+			$('#countdown').html('<span class="days">' + days + '</span>' + '<b>d</b><span class="hours">' + hours + '</span><b>h</b><span class="minutes">'
+	    + minutes + '</span><b>m</b><span class="seconds">' + seconds + '</span><b>s till</b>');
+		}
+		//else if (days < 0){
+		//	$('#countdown').html('<p>This event has ended.</p><p>来日后会相予期 去去莫迟疑﻿</p>')
+		//}
+
+
+}, 1000);
+
+//speaker click
+$(".speaker").click(function() {
+  window.location = $(this).find("a").attr("href");
+  return false;
+});
